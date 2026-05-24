81b5e85b613b0b8278dd6a94f79a8d34b40ab74f
Andres Beloqui <abeloqui@correo.ucu.edu.uy>
2026-05-23 23:17:43 -0300

    Agrego program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var fachada = new Fachada();
+
+fachada.RegistrarUsuario("Andres");
+
+fachada.AgregarItem("Song A", "Artista 1", new List<string> { "rock" });
+fachada.AgregarItem("Song B", "Artista 2", new List<string> { "pop" });
+
+var usuario = fachada.ObtenerUsuario("Andres");
+usuario.AgregarPreferencia("rock");
+
+var recomendaciones = fachada.Recomendar("Andres");
+
+foreach (var item in recomendaciones)
+{
+    Console.WriteLine(item.Nombre);
+}
