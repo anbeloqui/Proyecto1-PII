71dc8c10ef4d797ed28aaacfecc7183c5370d5d4
Andres Beloqui <abeloqui@correo.ucu.edu.uy>
2026-07-03 01:58:22 -0300

    Agrega tests de excepciones en fachada

---
+++ test/ProyectoPII.Tests/FachadaExcepcionesTests.cs
@@ -0,0 +1,51 @@
+using ProyectoPII.Excepciones;
+using Xunit;
+
+namespace ProyectoPII.Tests;
+
+public class FachadaExcepcionesTests
+{
+    [Fact]
+    public void RegistrarUsuarioLanzaExcepcionSiUsuarioYaExiste()
+    {
+        ProyectoPII.Fachada.Fachada fachada = new ProyectoPII.Fachada.Fachada();
+
+        fachada.RegistrarUsuario(1, "Andres");
+
+        Assert.Throws<ExcepcionUsuarioYaExiste>(() =>
+        {
+            fachada.RegistrarUsuario(2, "Andres");
+        });
+    }
+[Fact]
+public void AgregarPreferenciaLanzaExcepcionSiUsuarioNoExiste()
+{
+    ProyectoPII.Fachada.Fachada fachada = new ProyectoPII.Fachada.Fachada();
+
+    Assert.Throws<ExcepcionUsuarioNoEncontrado>(() =>
+    {
+        fachada.AgregarPreferencia("NoExiste", "rock");
+    });
+}
+[Fact]
+public void RecomendarLanzaExcepcionSiUsuarioNoExiste()
+{
+    ProyectoPII.Fachada.Fachada fachada = new ProyectoPII.Fachada.Fachada();
+
+    Assert.Throws<ExcepcionUsuarioNoEncontrado>(() =>
+    {
+        fachada.Recomendar("NoExiste");
+    });
+}
+
+[Fact]
+public void LikeLanzaExcepcionSiUsuarioNoExiste()
+{
+    ProyectoPII.Fachada.Fachada fachada = new ProyectoPII.Fachada.Fachada();
+
+    Assert.Throws<ExcepcionUsuarioNoEncontrado>(() =>
+    {
+        fachada.Like("NoExiste", 1);
+    });
+}
+}
